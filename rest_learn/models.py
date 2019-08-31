61de33a4f386df3b4c94a87621dac5f935c86358
DELL <3053366571@qq.com>
2019-08-31 16:19:25 +0800

    Initial commit

---
+++ rest_learn/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class TestModel(models.Model):
+    name  = models.CharField(max_length=20)
+    code  = models.CharField(max_length=20)
+    created_time = models.DateTimeField(auto_now_add=True)
+    change_time = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.name
+
